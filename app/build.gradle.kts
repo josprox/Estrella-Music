4f0fdc5a257e92f5a9b703e45b9445b8a84366bc
JOSPROX <pinguin69eyes@gmail.com>
2025-07-06 01:22:49 -0600

    Update verdions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.josprox.jossmusic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 46
-        versionName = "2.1.1"
+        versionCode = 47
+        versionName = "2.1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
