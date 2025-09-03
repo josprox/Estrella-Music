2439038cae2a042b992b176e5a39e78cb23c5fc8
josprox <pinguin69eyes@gmail.com>
2025-09-02 22:42:30 -0600

    New logic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 50
-        versionName = "2.1.6"
+        versionName = "2.1.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
