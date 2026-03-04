bdce08e76e7a392a0ad2606d3c53c116d9d16d6b
josprox <pinguin69eyes@gmail.com>
2026-03-03 20:14:42 -0600

    Features: Native support for Podcasts, Innertube refactor, and compilation bug fixes

--- app/src/main/java/com/zionhuang/music/ui/screens/search/OnlineSearchResult.kt
+++ app/src/main/java/com/zionhuang/music/ui/screens/search/OnlineSearchResult.kt
@@ -173,7 +173,7 @@ fun OnlineSearchResult(
                                 playerConnection.playQueue(YouTubeQueue.radio(item.asSongItem().toMediaMetadata()))
                             }
                         }
-                        is PodcastItem -> navController.navigate("online_playlist/${item.id}")
+                        is PodcastItem -> navController.navigate("podcast/${item.id}")
                     }
                 }
                 .animateItem()
