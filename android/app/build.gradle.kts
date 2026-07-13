f4325d2c5e9a8d5cb7413acd53412bc9363005bc
JOSPROX <pinguin69eyes@gmail.com>
2026-07-13 14:49:47 -0600

    refactor: remove ABI splits configuration from build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -73,14 +73,6 @@ android {
         }
     }
 
-    splits {
-        abi {
-            isEnable = true
-            reset()
-            include("armeabi-v7a", "arm64-v8a", "x86_64")
-            isUniversalApk = true
-        }
-    }
 }
 
 flutter {
